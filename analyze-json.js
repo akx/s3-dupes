e437c586c2bb62f44d9fe0448c143e1a034099fa
Aarni Koskela <akx@iki.fi>
2017-06-22 20:18:06 +0300

    Refactor reporting

--- analyze-json.js
+++ analyze-json.js
@@ -2,24 +2,14 @@ const argv = require('yargs').demandOption(['file']).argv;
 const fs = require('fs');
 const R = require('ramda');
 const getDuplicateGroups = require('./lib/get-duplicate-groups');
-const filesize = require('filesize');
+const DuplicateReporter = require('./lib/duplicate-reporter');
 
 const allObjects = new Map(JSON.parse(fs.readFileSync(argv.file, 'UTF-8')));
 
-let wastedBytes = 0;
-
-function reportDuplicates({objects, reason, size}) {
-  console.log(`*** size: ${filesize(size)} - ${reason}`);
-  objects.forEach((object) => {
-    console.log(object.Key);
-  });
-  console.log('');
-  wastedBytes += size * (objects.length - 1);
-}
-
+const reporter = new DuplicateReporter();
 getDuplicateGroups(allObjects.values()).then((groups) => {
   groups.forEach((group) => {
-    reportDuplicates(group);
+    reporter.printGroup(group);
   });
-  console.log(`**** total wasted: ${filesize(wastedBytes)}`);
+  reporter.printTotalWasted();
 });
