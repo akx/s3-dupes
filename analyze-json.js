334a1ff1e87b4d9be7d0b8a75765b800a32fbc6b
Aarni Koskela <akx@iki.fi>
2017-06-23 16:18:45 +0300

    Refactor reporter further to use promiseEach

--- analyze-json.js
+++ analyze-json.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 const R = require('ramda');
 const getDuplicateGroups = require('./lib/get-duplicate-groups');
 const DuplicateReporter = require('./lib/duplicate-reporter');
+const promiseEach = require('./lib/promise-each');
 
 const allObjects = new Map(JSON.parse(fs.readFileSync(argv.file, 'UTF-8')));
 
 const reporter = new DuplicateReporter();
-getDuplicateGroups(allObjects.values()).then((groups) => {
-  groups.forEach((group) => {
-    reporter.printGroup(group);
-  });
+getDuplicateGroups(allObjects.values())
+.then((groups) => promiseEach(groups, (group) => {
+  reporter.printGroup(group);
+}))
+.then(() => {
   reporter.printTotalWasted();
 });
