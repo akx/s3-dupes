7c733b121f9579f0c3858098dc4a71fc48ba3341
Aarni Koskela <akx@iki.fi>
2017-06-24 17:28:26 +0300

    Add include, exclude and interactive deletion mode

--- analyze-json.js
+++ analyze-json.js
@@ -1,16 +1,33 @@
-const argv = require('yargs').demandOption(['file']).argv;
+const AWS = require('aws-sdk');
 const fs = require('fs');
 const R = require('ramda');
-const getDuplicateGroups = require('./lib/get-duplicate-groups');
+
 const DuplicateReporter = require('./lib/duplicate-reporter');
+const getDuplicateGroups = require('./lib/get-duplicate-groups');
 const promiseEach = require('./lib/promise-each');
+const applyFilter = require('./lib/apply-filter');
+
+const argv = (
+  require('yargs')
+  .demandOption(['file'])
+  .option('interactive', {default: false})
+  .option('exclude', {alias: 'x', type: 'array', default: []})
+  .option('include', {alias: 'i', type: 'array', default: []})
+).argv;
+
+const s3 = new AWS.S3();
 
 const allObjects = new Map(JSON.parse(fs.readFileSync(argv.file, 'UTF-8')));
 
 const reporter = new DuplicateReporter();
-getDuplicateGroups(allObjects.values())
+applyFilter(allObjects.values(), argv.include, argv.exclude)
+.then(getDuplicateGroups)
+.then((groups) => R.sortBy((g) => -g.objects[0].Size, groups))
 .then((groups) => promiseEach(groups, (group) => {
-  reporter.printGroup(group);
+  reporter.printGroup(group, !argv.interactive);
+  if (argv.interactive) {
+    return reporter.promptDelete(group, s3);
+  }
 }))
 .then(() => {
   reporter.printTotalWasted();
